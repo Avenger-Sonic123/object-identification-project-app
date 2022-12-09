2f653f81adf9848252bcf1a8745fc5a8ec257bbc
Avenger-Sonic123 <79879253+Avenger-Sonic123@users.noreply.github.com>
2022-12-08 18:41:07 -0600

    Add files via upload

---
+++ bedroom.js
@@ -0,0 +1,54 @@
+img = "";
+var status = "";
+objects = [];
+
+function preload()
+{
+    img = loadImage('bedroom.jpg');
+}
+
+
+function setup()
+{
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function draw()
+{
+    image(img, 0, 20, 640, 420);
+    
+    if(status != "")
+    {
+        for(i = 0; i < objects.length; i++)
+        {
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+
+            fill("red");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("red");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded!");
+    status = true;
+    objectDetector.detect(img, gotResults);
+}
+
+function gotResults(error, results)
+{
+    if (error)
+    {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
